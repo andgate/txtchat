30a99a84d38b675544b330e39736bd391311a930
Gabriel Anderson <gabe@nixos-zetta.home>
2017-09-16 12:34:56 -0700

    produces docker images

--- src/App.hs
+++ src/App.hs
@@ -1,76 +1,73 @@
-{-# LANGUAGE  DataKinds
-            , DeriveGeneric
-            , LambdaCase
-            , TypeOperators
-  #-}
-module App where
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE OverloadedStrings #-}
 
+module App where
 
 import           Control.Monad.Trans.Except
 import           Data.Aeson
+import Data.Text
 import           GHC.Generics
 import           Network.Wai
 import           Network.Wai.Handler.Warp
 import           Servant
 import           System.IO
 
-
-
 -- * api
 
 type ItemApi =
+  "hello-world" :> Get '[JSON] Text :<|>
   "item" :> Get '[JSON] [Item] :<|>
   "item" :> Capture "itemId" Integer :> Get '[JSON] Item
 
-
 itemApi :: Proxy ItemApi
 itemApi = Proxy
-
 
 -- * app
 
 run :: IO ()
 run = do
-  let port = 3000
+  let port = 80
       settings =
         setPort port $
         setBeforeMainLoop (hPutStrLn stderr ("listening on port " ++ show port)) $
         defaultSettings
   runSettings settings =<< mkApp
 
-
 mkApp :: IO Application
 mkApp = return $ serve itemApi server
 
-
 server :: Server ItemApi
 server =
+  getHello :<|>
   getItems :<|>
   getItemById
 
 
+getHello :: Handler Text
+getHello = return "Hello world"
+
 getItems :: Handler [Item]
 getItems = return [exampleItem]
 
-
 getItemById :: Integer -> Handler Item
-getItemById = \case
+getItemById = \ case
   0 -> return exampleItem
   _ -> throwError err404
-
 
 exampleItem :: Item
 exampleItem = Item 0 "example item"
 
-
 -- * item
-data Item
-  = Item
-    { itemId :: Integer
-    , itemText :: String
-    }
-    deriving(Eq, Show, Generic)
 
+data Item
+  = Item {
+    itemId :: Integer,
+    itemText :: String
+  }
+  deriving (Eq, Show, Generic)
 
 instance ToJSON Item
 instance FromJSON Item
